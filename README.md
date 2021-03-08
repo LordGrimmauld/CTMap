1ae051dc8a968a2092d0bc10b83fed59e9d84126
grimmauld <soeren@benjos.de>
2021-03-08 16:54:55 +0100

    get stuff working, initial map version

---
+++ README.md
@@ -0,0 +1,2 @@
+***This is a little stupid project***
+The goal is to map the Create Technical server from various accounts and merging their journeymap data
---
+++ map_combiner.py
@@ -0,0 +1,31 @@
+from tkinter import filedialog
+import os
+from shutil import copyfile
+from PIL import Image
+
+files1 = list(filedialog.askopenfilenames(title="Map Sections", filetypes=[("Map Sections", ".png")]))
+files2 = list(filedialog.askopenfilenames(title="Map Sections", filetypes=[("Map Sections", ".png")]))
+
+out_path = filedialog.askdirectory(title="Save Map Sections at...")
+
+
+def merge(file1, file2, out):
+    print(f"merging {file1} + {file2} to {out}")
+    background = Image.open(file1)
+    foreground = Image.open(file2)
+    Image.alpha_composite(background, foreground).save(out)
+
+
+for file1 in set(files1) - set(files2):
+    filename1 = os.path.split(file1)[-1]
+    for file2 in files2[:]:
+        filename2 = os.path.split(file2)[-1]
+        if filename1 == filename2:
+            merge(file1, file2, os.path.join(out_path, filename1))
+            files2.remove(file2)
+            break
+    else:
+        copyfile(file1, os.path.join(out_path, os.path.split(file1)[-1]))
+
+for file1 in set(files2) - set(files1):
+    copyfile(file1, os.path.join(out_path, os.path.split(file1)[-1]))
