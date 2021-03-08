47d8257aaf909d1589079257e6fa79419b648822
grimmauld <soeren@benjos.de>
2021-03-08 18:03:43 +0100

    Add Triatachion map

--- map_combiner.py
+++ map_combiner.py
@@ -1,7 +1,7 @@
 from tkinter import filedialog
 import os
-from shutil import copyfile
-from PIL import Image
+from shutil import copyfile, SameFileError
+from PIL import Image, UnidentifiedImageError
 
 files1 = list(filedialog.askopenfilenames(title="Map Sections", filetypes=[("Map Sections", ".png")]))
 files2 = list(filedialog.askopenfilenames(title="Map Sections", filetypes=[("Map Sections", ".png")]))
@@ -11,9 +11,12 @@ out_path = filedialog.askdirectory(title="Save Map Sections at...")
 
 def merge(file1, file2, out):
     print(f"merging {file1} + {file2} to {out}")
-    background = Image.open(file1)
-    foreground = Image.open(file2)
-    Image.alpha_composite(background, foreground).save(out)
+    try:
+        background = Image.open(file1)
+        foreground = Image.open(file2)
+        Image.alpha_composite(background, foreground).save(out)
+    except UnidentifiedImageError:
+        pass
 
 
 for file1 in set(files1) - set(files2):
@@ -25,7 +28,13 @@ for file1 in set(files1) - set(files2):
             files2.remove(file2)
             break
     else:
-        copyfile(file1, os.path.join(out_path, os.path.split(file1)[-1]))
+        try:
+            copyfile(file1, os.path.join(out_path, os.path.split(file1)[-1]))
+        except SameFileError:
+            pass
 
-for file1 in set(files2) - set(files1):
-    copyfile(file1, os.path.join(out_path, os.path.split(file1)[-1]))
+for file1 in files2:
+    try:
+        copyfile(file1, os.path.join(out_path, os.path.split(file1)[-1]))
+    except SameFileError:
+        pass
